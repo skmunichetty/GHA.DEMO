c17f206d26db297d966178f5dfd95e9710390ffb
Sai Kiran <sai.buildxact1@gmail.com>
2024-04-25 00:10:24 +1000

    Added tests project and updated the workflow file to test variables

---
+++ .github/workflows/client.ps1
@@ -0,0 +1,7 @@
+# Accessing environment variables
+$logFilename = $env:logfilename
+$errorFilename = $env:errorFilename
+
+# Printing variables
+Write-Host "Log Filename: $logFilename"
+Write-Host "Error Filename: $errorFilename"
---
+++ gha.demo/gha.demo.tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace gha.demo.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
