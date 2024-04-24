2a3729aaf10c6e894d0627aecc342022ae01dc42
Sai Kiran <sai.buildxact1@gmail.com>
2024-04-25 00:35:19 +1000

    12

--- .github/scripts/client.ps1
+++ .github/scripts/client.ps1
@@ -1,14 +1,14 @@
 
 Write-Host 'In client.ps1 file.'
 
-# Redirect stderr to stdout
-$ErrorActionPreference = "Continue"
+# # Redirect stderr to stdout
+# $ErrorActionPreference = "Continue"
 
-# Accessing environment variables
-$logFilename = $env:logfilename
-$errorFilename = $env:errorFilename
+# # Accessing environment variables
+# $logFilename = $env:logfilename
+# $errorFilename = $env:errorFilename
 
-# Printing variables
+# # Printing variables
 Write-Host "Log Filename: $logFilename"
 Write-Host "Error Filename: $errorFilename"
 
